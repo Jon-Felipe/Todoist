d52ede56d159ab876bb6c517e836b9b3566fc199
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-08 17:47:23 +0200

    displaying a list of dummy todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,27 @@
 import Header from './components/Header';
 import Button from './components/Button';
 import Select from './components/Select';
-import TodoList from './components/TodoList';
+import TodoList, { Todo } from './components/TodoList';
+
+const dummyTodos: Todo[] = [
+  { id: 1, title: 'My first todo', date: new Date(), completed: false },
+  { id: 2, title: 'My second todo', date: new Date(), completed: false },
+  { id: 3, title: 'My third todo', date: new Date(), completed: true },
+];
 
 function App() {
   return (
     <main className='flex items-center justify-center h-screen w-screen bg-violet-100'>
       <div className='w-full max-w-[800px] text-center'>
-        <section className='mb-8'>
+        <section className='mb-10'>
           <Header title='Todoist' />
         </section>
-        <section className='flex items-center justify-between'>
+        <section className='flex items-center justify-between mb-4'>
           <Button text='Add Task' onClick={() => console.log('add todo')} />
           <Select />
         </section>
         <section>
-          <TodoList />
+          <TodoList todo={dummyTodos} />
         </section>
       </div>
     </main>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,52 @@
-type TodoListProps = {};
+import { FaTrash, FaPen } from 'react-icons/fa';
 
-function TodoList({}: TodoListProps) {
-  return <div>TodoList</div>;
+export interface Todo {
+  id: string | number;
+  title: string;
+  date: Date;
+  completed: boolean;
+}
+
+type TodoListProps = {
+  todo: Todo[];
+};
+
+function TodoList({ todo }: TodoListProps) {
+  return (
+    <div className='bg-violet-200 p-8 rounded-xl'>
+      {todo.map((todo) => (
+        <div key={todo.id} className='py-4 mb-4 bg-white rounded-xl shadow-sm'>
+          <div className='flex items-center justify-between px-4'>
+            <div className='flex items-center'>
+              <input
+                type='checkbox'
+                name=''
+                id=''
+                checked={todo.completed}
+                className='w-6 h-6'
+              />
+              <div className='text-left ml-6'>
+                <h3 className='text-xl font-bold text-neutral-500'>
+                  {todo.title}
+                </h3>
+                <p className='text-sm font-semibold text-neutral-500'>
+                  {todo.date.toDateString()}
+                </p>
+              </div>
+            </div>
+            <div className='flex items-center gap-x-2'>
+              <button className='bg-neutral-200 p-3 rounded-lg cursor-pointer'>
+                <FaTrash />
+              </button>
+              <button className='bg-neutral-200 p-3 rounded-lg cursor-pointer'>
+                <FaPen />
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default TodoList;

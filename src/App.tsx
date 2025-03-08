04bf2f6a9ab16f8d2df92044fe3b8f7fe059bf19
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-08 14:02:11 +0200

    select component init

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 // components
 import Header from './components/Header';
 import Button from './components/Button';
+import Select from './components/Select';
 
 function App() {
   return (
     <main className='flex items-center justify-center h-screen w-screen bg-violet-100'>
       <div className='w-full max-w-[800px] text-center'>
-        <section className='mb-6'>
+        <section className='mb-8'>
           <Header title='Todoist' />
         </section>
-        <section>
+        <section className='flex items-center justify-between'>
           <Button text='Add Task' onClick={() => console.log('add todo')} />
+          <Select />
         </section>
       </div>
     </main>

00548e93829db7ba865d9a057f6994feb7431c84
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-08 14:09:53 +0200

    select component styling and option items

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,7 +1,17 @@
 type SelectProps = {};
 
 function Select({}: SelectProps) {
-  return <div>Select</div>;
+  return (
+    <select
+      name='progress'
+      id='progress'
+      className='bg-violet-50 px-4 py-2 rounded-lg shadow-md text-lg font-semibold focus:outline-none'
+    >
+      <option value='all'>All</option>
+      <option value='completed'>Completed</option>
+      <option value='uncompleted'>Uncompleted</option>
+    </select>
+  );
 }
 
 export default Select;

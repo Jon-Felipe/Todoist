ed2b6b5170f785544e08a322b25d3be8151cd3ed
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-08 13:42:55 +0200

    app body styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <main>
-      <Header />
+    <main className='flex items-center justify-center h-screen w-screen bg-violet-100'>
+      <Header title='Todoist List' />
     </main>
   );
 }

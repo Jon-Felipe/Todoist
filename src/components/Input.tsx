e0dd9199641f7e8c90054ff5d4c6f46bef0924fd
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-09 13:58:55 +0200

    input component styling and props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-type Props = {};
+type InputProps = {
+  type: React.InputHTMLAttributes<HTMLInputElement>['type'];
+  placeholder?: React.InputHTMLAttributes<HTMLInputElement>['placeholder'];
+  name: string;
+  value: string | number;
+};
 
-function Input({}: Props) {
-  return <div>Input</div>;
+function Input({ type, placeholder, name, value }: InputProps) {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      name={name}
+      value={value}
+      className='border border-neutral-300 p-2 rounded-md w-full'
+    />
+  );
 }
 
 export default Input;

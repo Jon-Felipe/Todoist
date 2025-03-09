eb0e87efcf3cc0169b8556b48f5711f5523a1c25
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-09 14:01:25 +0200

    add todo component withe styling and input and butt on components

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+// components
+import Input from './Input';
+import Button from './Button';
 
-type Props = {};
+type AddTodoProps = {};
 
-function AddTodo({}: Props) {
-  return <div>AddTodo</div>;
+function AddTodo({}: AddTodoProps) {
+  return (
+    <div className='border border-neutral-300 rounded-md p-6'>
+      <div className='flex items-center gap-x-8'>
+        <div className='w-md'>
+          <Input type='text' placeholder='Todo Title' name='todo' value='' />
+        </div>
+        <div className='w-md'>
+          <Input
+            type='text'
+            placeholder='Todo Description'
+            name='todoDescription'
+            value=''
+          />
+        </div>
+        <Button text='Add Todo' onClick={() => console.log('first')} />
+      </div>
+    </div>
+  );
 }
 
 export default AddTodo;

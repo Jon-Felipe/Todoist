6c1812ad270ef6fea92eb29cefb878a75d3c5827
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-10 18:39:15 +0200

    input component on change handler

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,15 +5,17 @@ type InputProps = {
   placeholder?: React.InputHTMLAttributes<HTMLInputElement>['placeholder'];
   name: string;
   value: string | number;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 };
 
-function Input({ type, placeholder, name, value }: InputProps) {
+function Input({ type, placeholder, name, value, onChange }: InputProps) {
   return (
     <input
       type={type}
       placeholder={placeholder}
       name={name}
       value={value}
+      onChange={onChange}
       className='border border-neutral-300 p-2 rounded-md w-full'
     />
   );

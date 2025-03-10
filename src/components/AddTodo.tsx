f0e24f616545827a9bf606bb3fb4aedaa32ae884
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-10 18:39:54 +0200

    add todo component on change functionality and add new todo

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,25 +1,57 @@
+import React, { useState } from 'react';
+
 // components
 import Input from './Input';
 import Button from './Button';
+import { Todo } from './TodoList';
 
-type AddTodoProps = {};
+type AddTodoProps = {
+  onHandleAddTodo: (todo: Partial<Todo>) => void;
+};
 
-function AddTodo({}: AddTodoProps) {
+function AddTodo({ onHandleAddTodo }: AddTodoProps) {
+  const [todo, setTodo] = useState<Partial<Todo>>({
+    title: '',
+    description: '',
+  });
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setTodo((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  }
+
   return (
     <div className='border border-neutral-300 rounded-md p-6'>
       <div className='flex items-center gap-x-8'>
         <div className='w-md'>
-          <Input type='text' placeholder='Todo Title' name='todo' value='' />
+          <Input
+            type='text'
+            placeholder='Todo Title'
+            name='title'
+            value={todo?.title || ''}
+            onChange={handleChange}
+          />
         </div>
         <div className='w-md'>
           <Input
             type='text'
             placeholder='Todo Description'
-            name='todoDescription'
-            value=''
+            name='description'
+            value={todo?.description || ''}
+            onChange={handleChange}
           />
         </div>
-        <Button text='Add Todo' onClick={() => console.log('first')} />
+        <Button
+          text='Add Todo'
+          onClick={() => {
+            onHandleAddTodo(todo);
+            setTodo({ title: '', description: '' });
+          }}
+        />
       </div>
     </div>
   );
